c1f024ae61f0f3f51527f2f44122444fa7061996
TBFMcDuck <124277986+TBFMcDuck@users.noreply.github.com>
2023-02-25 11:47:58 +0100

    Update mainJS.js

--- JS/mainJS.js
+++ JS/mainJS.js
@@ -1,4 +1,7 @@
-
-function test() {
-    console.log("Test")
-}
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+    // true for mobile device
+    document.write("mobile device");
+  }else{
+    // false for not mobile device
+    document.write("not mobile device");
+  }
